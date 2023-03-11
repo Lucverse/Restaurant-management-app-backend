ff3926a60766625c2366ec809b46d607540573b3
Luc <Luc1fer.tms.ig@gmail.com>
2023-03-11 23:08:48 +0530

    1

--- models/User.js
+++ models/User.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-const User = mongoose.model(
-    "User",
+const User = mongoose.model("User",
     new mongoose.Schema({
         firstName: {
             type: String,
